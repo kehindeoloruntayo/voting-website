2e0d854f638f6100dc2b55b4d039414cdf2ba0a7
kehindeoloruntayo <kehindeoloruntayo1@gmail.com>
2023-05-05 00:34:15 +0100

    add javascript folder

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $('#age-form').submit(function(event) {
+    event.preventDefault();
+    const age = parseInt($('#age').val());
+    if (age >= 18) {
+      window.location.href = "legal.html";
+    } else {
+      window.location.href = "underage.html";
+    }
+  });
+});
